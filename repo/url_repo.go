e097d3c207138bd4c79b20fc48a3155fb9f2019a
RAY <ray13242000@gmail.com>
2021-10-11 00:13:54 +0800

    add repo

---
+++ repo/url_repo.go
@@ -0,0 +1,28 @@
+package repo
+
+import (
+	"time"
+
+	"github.com/Ray0427/url-shortener/model"
+	"gorm.io/gorm"
+)
+
+type UrlRepo struct {
+	DB *gorm.DB
+}
+
+func NewUrlRepo(DB *gorm.DB) UrlRepo {
+	return UrlRepo{DB: DB}
+}
+
+func (ur *UrlRepo) CreateUrl(url string, expireAt time.Time) model.Url {
+	u := model.Url{FullUrl: url, ExpireAt: expireAt}
+	ur.DB.Create(&u)
+	return u
+}
+
+func (ur *UrlRepo) GetUrl(id uint) model.Url {
+	var u model.Url
+	ur.DB.First(&ur, id)
+	return u
+}
