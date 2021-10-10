3ba66470e9c30e877da89d056753e621ec981a1d
RAY <ray13242000@gmail.com>
2021-10-10 17:21:36 +0800

    add dotenv config add db & orm

--- go.mod
+++ go.mod
@@ -3,9 +3,12 @@ module github.com/Ray0427/url-shortener
 go 1.14
 
 require (
+	github.com/caarlos0/env/v6 v6.7.1
 	github.com/gin-gonic/gin v1.7.4
 	github.com/go-playground/validator/v10 v10.9.0 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/go-cmp v0.5.6 // indirect
+	github.com/joho/godotenv v1.4.0
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
@@ -13,6 +16,9 @@ require (
 	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
 	golang.org/x/sys v0.0.0-20211007075335-d3039528d8ac // indirect
 	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 	google.golang.org/protobuf v1.27.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gorm.io/driver/mysql v1.1.2
+	gorm.io/gorm v1.21.16
 )
--- main.go
+++ main.go
@@ -1,13 +1,61 @@
 package main
 
 import (
+	"fmt"
+	"log"
+
 	"github.com/Ray0427/url-shortener/handlers"
+	"github.com/caarlos0/env/v6"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
-func main() {
+type Config struct {
+	Database struct {
+		USERNAME string `env:"DB_USERNAME"`
+		PASSWORD string `env:"DB_PASSWORD"`
+		NETWORK  string `env:"DB_NETWORK" envDefault:"tcp"`
+		SERVER   string `env:"DB_ADDRESS"`
+		PORT     string `env:"DB_PORT" envDefault:"3306"`
+		DATABASE string `env:"DB_DATABASE"`
+	}
+}
+
+func initConfig() Config {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	config := Config{}
+	if err := env.Parse(&config); err != nil {
+		log.Fatalf("%+v\n", err)
+	}
+	// log.Printf("%+v\n", config)
+	return config
+}
+
+func initRouter() *gin.Engine {
 	r := gin.Default()
 	r.POST("/api/v1/urls", handlers.PostUrls)
 	r.GET("/:url_id", handlers.GetId)
 	r.Run()
+	return r
+}
+
+func initDatabase(config Config) *gorm.DB {
+	dsn := fmt.Sprintf("%s:%s@%s(%s:%s)/%s?charset=utf8mb4&parseTime=True", config.Database.USERNAME, config.Database.PASSWORD, config.Database.NETWORK, config.Database.SERVER, config.Database.PORT, config.Database.DATABASE)
+	// log.Println(dsn)
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Error connect Database with gorm")
+	}
+	return db
+}
+
+func main() {
+	config := initConfig()
+	initDatabase(config)
+	initRouter()
 }
